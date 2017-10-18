20429576ac921fe2f653da9b5e5fae5a44dd17d2
jaym29 <32370809+jaym29@users.noreply.github.com>
2017-10-17 22:23:28 -0500

    Add files via upload

---
+++ work.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+
+def main () :
+	f = open('http_access_log.log', 'r')
+	count = 0
+	allLine =[]
+	by_weekday = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
+	by_month = {}
+		
+for line in f:
+		count += 1
+		allLine.append(line) 
+		split_data = line.split() 
+				
+try:
+	str_date = line.split() [3] (1:].split(':') [0]
+except:
+	pass
+date = datetime.strptime( str_date, "%d/%b/%Y" ) 
+by_weekday[date.weekday() ] += 1
+				
+if count > 2000:
+	print by_weekday
+	return 0
+						
+print ("1. How many total request were made in the time period represented in the log?") 
+print len(allLine)
+	
+	
+main () 
